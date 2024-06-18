34b77ae4469edd74195ff9e2d7ed2f3cfe9385d2
Jiang <wishjiangnan@gmail.com>
2024-06-18 23:53:04 +0630

    Updated database Ip in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'bigsmile',
         'USER': 'root',
         'PASSWORD': 'admin12345',
-        'HOST': '18.140.62.169',
+        'HOST': '3.0.99.13',
         'POST': '3306'
     }
 }
